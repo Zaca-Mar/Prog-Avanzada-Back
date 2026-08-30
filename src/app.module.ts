8a139b8e62849f41cf35c6719a1ed50cdb56536f
Zacarias Martines <134318060+Zaca-Mar@users.noreply.github.com>
2026-08-30 16:27:27 -0300

    creacion de tablas aut

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,7 +47,7 @@ import { BusquedasModule } from './modules/gestion-documentos/busquedas/busqueda
       // TypeOrmModule.forFeature([Entidad]) en tus módulos
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       // entities,
-      synchronize: true, 
+      synchronize: true, // Solo para desarrollo, no usar en producción
       ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : false,
     }),
 
